8db9a5f240e68334b9acb5aefeb66a008bce352b
Bastian Bechtold <bastibe.dev@mailbox.org>
2024-09-19 20:21:40 +0200

    Update to the current version
    I tinkered with this script quite a bit, though mostly on my own machine, without pushing regularly to git. So here's an update.
    
    As of 4.8, we no longer need a custom user.css.
    
    I re-rendered all the styles with my new X-T5, and added a few missing film sims.
    
    I repainted all the icons, based on the film simulation images found in the X-Pro3.
    
    The film sims are no longer specialized by camera model. Clicking a button now just applies a style, nothing more.

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ A graphical panel in darktable's lighttable and darkroom for applying presets.
 This darktable extension consists of
 
 - a Lua script that adds a "Film Simulations" panel to the darkroom and lighttable
-- a *user.css* and a number of icons for putting images on the panel's buttons
 - a number of styles, which the buttons apply
 
 The supplied styles are:
@@ -18,6 +17,10 @@ The supplied styles are:
 - Fuji/Pro Neg High
 - Fuji/Pro Neg Std
 - Fuji/Eterna
+- Fuji/Bleach Bypass
+- Fuji/Classic Neg
+- Fuji/Nostalgic Neg
+- Fuji/Reala Ace
 - Ricoh/Ricoh Standard
 - Ricoh/Positive Film
 - Ricoh/Negative Film
@@ -29,57 +32,45 @@ The supplied styles are:
 - 4 × Exposure (simple exposure adjustments)
 - DR100, DR200, DR400 (tone equalizer presets for raising shadows/midtones by 0/1/2 EV)
 
-Not all styles are applicable to all images. *Fuji* and *Ricoh* styles are targeted to Fuji/Ricoh cameras, and won't apply to others. This is implemented by simple EXIF-maker matching with the style prefix (`Fuji/`, `Ricoh/`) in the lua script.
+The Fuji and Ricoh styles were created using `darktable-chart` and a color checker photo. Darktable-chart can create darktable styles for emulating the look of rendered JPEG. In this case, I took a picture of a color checker, and converted it in my Fuji and Ricoh camera to the various film simulations. The darktable source render uses Sigmoid with preserve hue at 50%.
 
 Adding a new button to the Film Simulations panel involves:
-- adding the button in *FilmSimPanel.lua* using `darktable.new_widget`, and giving it a unique `name`
-- adding the button name to the class at the beginning of *user.css* to style it as a panel button
-- adding a unique entry at the end of *user.css* to give it an icon
+- adding the button in *FilmSimPanel.lua* using `darktable.new_widget`, and giving it a `apply_style_to_selection("yourstylename")` and `image`.
+- adding a style with a matching name
+- adding an icon that matches the `image`
 
 ## Installation
 
-Start darktable and use import action of the styles panel in the lighttable view.
-Select all files in the *styles* directory of this repository for import.
+Copy the contents of the repository (excluding *README.md* and *screenshot.png* and *styles_source*, although they won't hurt) to your darktable configuration directory (e.g. *~/.config/darktable* or *~/.var/app/org.darktable.Darktable/config/darktable*).
 
-After that, the styles should be already applicable.
-
-Close darktable (or make sure you restart the application at the end of installation).
+Start darktable and use import action of the styles panel in the lighttable view, then select all files in the *styles* directory of this repository for import. After that, the styles should be already applicable. Close darktable (or make sure you restart the application at the end of installation).
 
 The directories *icons* and *lua* must be copied to darktable configuration directory (e.g. *~/.config/darktable*, *~/.var/app/org.darktable.Darktable/config/darktable* or *C:\Users\\\<username>\AppData\Local\darktable*).
-
-If you have changed *user.css* e.g. in the general settings tab of darktable preferences, you have to add the content of *user.css* manually.
-
-Otherwise, the unmodified standard *user.css* in darktable configuration directory can be overwritten with the file *user.css* of this repositoy.
 
 You may still need to create *luarc* in the configuration directory.
 
 Finally, activate the FilmSimPanel lua script by adding this to *luarc*:
 
-> require "contrib/FilmSimPanel"
+    > require "contrib/FilmSimPanel"
 
 (This description should work for darktable 4, but at the time of writing has only been tested with darktable 4.4.2 on the current Linux Arch OS.)
 
 ## Usage
 
-The styles are meant to be applied after Sigmoid with 1.5 Contrast, 0 skew, per-color processing with 50% preserve hue (as tested in darktable 4.2.1).
-
-## Known Issues
-
-In some versions of Darktable, applying a style using `darktable.styles.apply` in the darkroom crashes darktable. The reasons for this are as of yet unknown. A [bug report](https://github.com/darktable-org/darktable/issues/13985) has been filed. You can apply the styles without issue from the styles menu, or in the lighttable panel.
-
-The Film Simulation Panel currently works around this by using the `darktable.gui.action` interface instead if in the darkroom view. This might behave strangely in edge cases, such as multiple images in the current selection, or if the style names contain untested characters (`/` is appropriately escaped, but others might need escaping as well).
+The styles are meant to be applied after Sigmoid with 1.5 Contrast, 0 skew, per-color processing with 50% preserve hue (as tested in darktable 4.8).
 
 ## Background
 
 The styles were compiled using *darktable-chart*:
 
 - edit your *darktablerc* and set `allow_lab_output=TRUE`
-- export each of the JPEGs in *style_source* as PFM (only available with the above edit)
-- export the two RAF/DNG as PFM as well, make sure white balance is set on the middle grey patch, and that the input profile is in its default state, and that only a bare minimum of modules is active
+- export each of the JPEGs in *style_source* as PFM, with profile Lab (only available with the above edit)
+- export the two RAF/DNG as PFM as well, make sure that white balance and exposure matches the JPEGs, and that the input profile is in its default state, and that only a bare minimum of modules is active
 - run `darktable-chart` or `flatpak run --command=darktable-chart org.darktable.Darktable`
 - select the exported RAF/DNG and the supplied *color_checker.cht* in the first tab (and adjust boundaries to match image, increase scale if necessary)
 - select the exported JPEG in the second tab (also adjust boundaries)
 - calculate and export in the third tab. If the difference is inf, check the masks in the first and second tab. I like to export only tone curve and color lookup table. Don't forget to set a style name.
+- Restart darktable-chart, as it won't work correctly if you export more than once
 
 The supplied target images were shot in sideways midday sunlight, white-balanced on a sheet of white paper. They were processed in-camera to the various film simulations.
 
--- lua/contrib/FilmSimPanel.lua
+++ lua/contrib/FilmSimPanel.lua
@@ -9,22 +9,10 @@ local function find_style(style_name)
     return nil
 end
 
-
 local function apply_style(style_name, image)
     local style = find_style(style_name)
     darktable.print("applying style " .. style_name)
-    if darktable.gui.current_view() == darktable.gui.views.lighttable then
-        darktable.styles.apply(style, image)
-    elseif darktable.gui.current_view() == darktable.gui.views.darkroom then
-        -- for some reason, darktable.styles.apply crashes darktable
-        -- half the time. Thus, use darktable.gui.action instead (and
-        -- escape forbidden characters in the style name)
-        darktable.gui.action("global/styles/" .. style_name:gsub("/", "-"), 0, "", "", 1.0)
-    else
-        darktable.print_error("[FilmSimPanel] could not apply style in " ..
-                              tostring(darktable.gui.current_view) .. " view")
-        return
-    end
+    darktable.styles.apply(style, image)
     darktable.print_log("[FilmSimPanel] applying style " .. style_name)
 end
 
@@ -32,20 +20,15 @@ end
 local function apply_style_to_selection(style_name)
     local images = darktable.gui.selection()
     if #images == 0 then
-        darktable.print(_("Please select an image"))
+        darktable.print("Please select an image")
     else
         for idx, image in ipairs(images) do
             local extension = image.filename:lower():match("%.[a-z]+$")
-            if extension == ".raf" and image.exif_maker == "FUJIFILM" and find_style("Fuji/" .. style_name) then
-                apply_style("Fuji/" .. style_name, image)
-            elseif extension == ".dng" and image.exif_maker == "RICOH IMAGING COMPANY, LTD." and find_style("Ricoh/" .. style_name) then
-                apply_style("Ricoh/" .. style_name, image)
-            elseif find_style(style_name) then
+            if find_style(style_name) then
                 apply_style(style_name, image)
             else
                 darktable.print("no style " .. style_name)
-                darktable.print_error("[FilmSimPanel] could not locate style " .. style_name ..
-                                      " for " .. image.exif_maker .. " image")
+                darktable.print_error("[FilmSimPanel] could not locate style " .. style_name)
             end
         end
     end
@@ -67,186 +50,237 @@ darktable.register_lib(
         darktable.new_widget("box") {
             orientation = "horizontal",
             darktable.new_widget("button") {
-                label = "Provia",
                 tooltip = "This is your camera’s default Film Simulation mode and is a good general-purpose setting that’s suitable for most subjects. It provides faithful color reproduction with moderate contrast and sharpness.",
                 name = "film_sim_panel_provia_button",
+                image = darktable.configuration.config_dir .. "/icons/provia.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Provia")
                 end
             },
             darktable.new_widget("button") {
-                label = "Astia",
                 tooltip = "Like the Velvia Film Simulation mode, ASTIA also delivers vibrant colors with extra saturation, but its contrast is softer, making this a good choice for fashion, interiors, and even portraits. The original FUJICHROME ASTIA boasted the softest tonal reproduction in its class, and was able to reproduce the most delicate of details.",
-                name = "film_sim_panel_astia_button",
+                name = "film_sim_panel_astia_button",                
+                image = darktable.configuration.config_dir .. "/icons/astia.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Astia")
                 end
             },
             darktable.new_widget("button") {
-                label = "Velvia",
                 tooltip = "One of our most famous films is FUJICHROME Velvia: a reversal film (ie one that produces transparencies or ‘slides’) that delivers vibrant images with lots of impact. The Film Simulation mode does the same, with a boost in color saturation and contrast that is great for landscape and nature photography.",
                 name = "film_sim_panel_velvia_button",
+                image = darktable.configuration.config_dir .. "/icons/velvia.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Velvia")
                 end
             },
             darktable.new_widget("button") {
-                label = "Classic Chrome",
                 tooltip = "This Film Simulation mode is not named after a specific FUJICHROME or FUJICOLOR product, but instead mimics the ambience of documentary-style magazine photography from decades past. It has slightly desaturated colors (especially reds and greens) with harder shadow contrast, resulting in a timeless, subdued look that works well with dramatic lighting and retro-style subjects, as well as reportage projects.",
                 name = "film_sim_panel_classic_chrome_button",
+                image = darktable.configuration.config_dir .. "/icons/classic chrome.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Classic Chrome")
                 end
             },
         },
         darktable.new_widget("box") {
             orientation = "horizontal",
             darktable.new_widget("button") {
-                label = "Pro Neg High",
                 tooltip = "Designed for fashion and portrait photography, PRO Neg. Hi features soft skin tones, but offers a little more color saturation and harder shadows than its sibling, PRO Neg. Std. It’s great for emphasizing line, form, and texture, and offers slightly enhanced contrast across the board.",
                 name = "film_sim_panel_pro_neg_high_button",
+                image = darktable.configuration.config_dir .. "/icons/pro neg high.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Pro Neg High")
                 end
             },
             darktable.new_widget("button") {
-                label = "Pro Neg Std",
                 tooltip = "This Film Simulation mode is a good choice for portraits, as it’s known for delivering soft contrast and beautiful skin tones. Colors are somewhat muted, making it a good option for lifestyle images and even street photography. This is a subtle option, able to record the most delicate of differences between colors and works well when the photographer has complete control over lighting.",
                 name = "film_sim_panel_pro_neg_std_button",
+                image = darktable.configuration.config_dir .. "/icons/pro neg std.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Pro Neg Std")
                 end
             },
             darktable.new_widget("button") {
-                label = "Eterna",
                 tooltip = "One for the filmmakers, ETERNA is named after the classic film industry emulsion of the same name. It delivers understated colors, extended dynamic range, and flat contrast. A perfect choice when color grading in post-production is desired.",
                 name = "film_sim_panel_eterna_button",
+                image = darktable.configuration.config_dir .. "/icons/eterna.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Fuji/Eterna")
                 end
             },
             darktable.new_widget("button") {
-                label = "Ricoh Standard",
+                tooltip = "Ricoh Standard",
+                name = "film_sim_panel_eterna_bleach_bypass_button",
+                image = darktable.configuration.config_dir .. "/icons/eterna bleach bypass.svg",
+                clicked_callback = function(source)
+                    apply_style_to_selection("Fuji/Eterna Bleach Bypass")
+                end
+            },
+        },
+        darktable.new_widget("box") {
+            orientation = "horizontal",
+            darktable.new_widget("button") {
+                tooltip = "Designed for fashion and portrait photography, PRO Neg. Hi features soft skin tones, but offers a little more color saturation and harder shadows than its sibling, PRO Neg. Std. It’s great for emphasizing line, form, and texture, and offers slightly enhanced contrast across the board.",
+                name = "film_sim_panel_classic_neg_button",
+                image = darktable.configuration.config_dir .. "/icons/classic neg.svg",
+                clicked_callback = function(source)
+                    apply_style_to_selection("Fuji/Classic Neg")
+                end
+            },
+            darktable.new_widget("button") {
+                tooltip = "This Film Simulation mode is a good choice for portraits, as it’s known for delivering soft contrast and beautiful skin tones. Colors are somewhat muted, making it a good option for lifestyle images and even street photography. This is a subtle option, able to record the most delicate of differences between colors and works well when the photographer has complete control over lighting.",
+                name = "film_sim_panel_nostalgic_neg_button",
+                image = darktable.configuration.config_dir .. "/icons/nostalgic neg.svg",
+                clicked_callback = function(source)
+                    apply_style_to_selection("Fuji/Nostalgic Neg")
+                end
+            },
+            darktable.new_widget("button") {
+                tooltip = "One for the filmmakers, ETERNA is named after the classic film industry emulsion of the same name. It delivers understated colors, extended dynamic range, and flat contrast. A perfect choice when color grading in post-production is desired.",
+                name = "film_sim_panel_reala_ace_button",
+                image = darktable.configuration.config_dir .. "/icons/reala ace.svg",
+                clicked_callback = function(source)
+                    apply_style_to_selection("Fuji/Reala Ace")
+                end
+            },
+            darktable.new_widget("button") {
+                tooltip = "Ricoh Standard",
                 name = "film_sim_panel_ricoh_button",
+                image = darktable.configuration.config_dir .. "/icons/ricoh.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Ricoh/Ricoh Standard")
                 end
             },
         },
         darktable.new_widget("box") {
             orientation = "horizontal",
             darktable.new_widget("button") {
-                label = "Positive Film",
+                tooltip = "Positive Film",
                 name = "film_sim_panel_positive_film_button",
+                image = darktable.configuration.config_dir .. "/icons/positive film.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Ricoh/Positive Film")
                 end
             },
             darktable.new_widget("button") {
-                label = "Negative Film",
+                tooltip = "Negative Film",
                 name = "film_sim_panel_negative_film_button",
+                image = darktable.configuration.config_dir .. "/icons/negative film.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Ricoh/Negative Film")
                 end
             },
             darktable.new_widget("button") {
-                label = "Vivid",
+                tooltip = "Vivid",
                 name = "film_sim_panel_vivid_button",
+                image = darktable.configuration.config_dir .. "/icons/vivid.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Ricoh/Vivid")
                 end
             },
             darktable.new_widget("button") {
-                label = "Retro",
+                tooltip = "Retro",
                 name = "film_sim_panel_retro_button",
+                image = darktable.configuration.config_dir .. "/icons/retro.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Ricoh/Retro")
                 end
             },
         },
         darktable.new_widget("box") {
             orientation = "horizontal",
             darktable.new_widget("button") {
-                label = "DR100",
+                tooltip = "DR100",
                 name = "film_sim_panel_dr100_button",
+                image = darktable.configuration.config_dir .. "/icons/dr100.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("DR100")
                 end
             },
             darktable.new_widget("button") {
-                label = "DR200",
+                tooltip = "DR200",
                 name = "film_sim_panel_dr200_button",
+                image = darktable.configuration.config_dir .. "/icons/dr200.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("DR200")
                 end
             },
             darktable.new_widget("button") {
-                label = "DR400",
+                tooltip = "DR400",
                 name = "film_sim_panel_dr400_button",
+                image = darktable.configuration.config_dir .. "/icons/dr400.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("DR400")
                 end
             },
         },
         darktable.new_widget("box") {
             orientation = "horizontal",
             darktable.new_widget("button") {
-                label = "Shadows+1",
+                tooltip = "Shadows+1",
                 name = "film_sim_panel_shadows_plus_one_button",
+                image = darktable.configuration.config_dir .. "/icons/shadows+1.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Shadows+1")
                 end
             },
             darktable.new_widget("button") {
-                label = "Shadows=0",
+                tooltip = "Shadows=0",
                 name = "film_sim_panel_shadows_zero_button",
+                image = darktable.configuration.config_dir .. "/icons/shadows=0.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Shadows=0")
                 end
             },
             darktable.new_widget("button") {
-                label = "Shadows-1",
+                tooltip = "Shadows-1",
                 name = "film_sim_panel_shadows_minus_one_button",
+                image = darktable.configuration.config_dir .. "/icons/shadows-1.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Shadows-1")
                 end
             },
             darktable.new_widget("button") {
-                label = "Shadows-2",
+                tooltip = "Shadows-2",
                 name = "film_sim_panel_shadows_minus_two_button",
+                image = darktable.configuration.config_dir .. "/icons/shadows-2.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Shadows-2")
                 end
             },
         },
         darktable.new_widget("box") {
             orientation = "horizontal",
             darktable.new_widget("button") {
-                label = "Exposure+1",
+                tooltip = "Exposure+1",
                 name = "film_sim_panel_exposure_plus_one_button",
+                image = darktable.configuration.config_dir .. "/icons/exposure+1.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Exposure+1")
                 end
             },
             darktable.new_widget("button") {
-                label = "Exposure+0.5",
+                tooltip = "Exposure+0.5",
                 name = "film_sim_panel_exposure_plus_half_button",
+                image = darktable.configuration.config_dir .. "/icons/exposure+0.5.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Exposure+0.5")
                 end
             },
             darktable.new_widget("button") {
-                label = "Exposure=0",
+                tooltip = "Exposure=0",
                 name = "film_sim_panel_exposure_zero_button",
+                image = darktable.configuration.config_dir .. "/icons/exposure=0.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Exposure=0")
                 end
             },
             darktable.new_widget("button") {
-                label = "Exposure-0.5",
+                tooltip = "Exposure-0.5",
                 name = "film_sim_panel_exposure_minus_half_button",
+                image = darktable.configuration.config_dir .. "/icons/exposure-0.5.svg",
                 clicked_callback = function(source)
-                    apply_style_to_selection(source.label)
+                    apply_style_to_selection("Exposure-0.5")
                 end
             },
         },
